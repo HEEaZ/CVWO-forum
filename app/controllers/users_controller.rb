86aeceedf6bc874e3a3eebac3280efcc9edb85d6
Imman <do.u.noe.imm@gmail.com>
2023-01-16 12:14:42 +0800

    Added authentication feature

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+    skip_before_action :authenticate_request, only: [:create]
+    before_action :set_user, only: [:show, :destroy]
+
+    def index 
+        @users = User.all
+        render json: @users, status: :ok
+    end
+
+    def show
+        render json: @user, status: :ok
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: { errors: @user.errors.full_messages },
+                status: :unprocessable_entity
+        end
+    end
+
+    def update
+        unless @user.update(user_params)
+            render json: { errors: @user.errors.full_messages },
+                status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @user.destroy
+    end
+
+    private
+    def user_params
+        params.permit(:username, :email, :password, :name)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+end
