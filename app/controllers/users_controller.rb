3c3f6436c569a7acfdafb92a7cd073315fb49235
Imman <do.u.noe.imm@gmail.com>
2023-01-21 15:08:10 +0800

    Fixed user create: now requires password confirmation and unique username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.permit(:username, :email, :password, :name)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation, :name)
     end
 
     def set_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,8 @@
 class User < ApplicationRecord
-    require "securerandom"
-
     has_many :posts, dependent: :destroy
     has_many :comments, dependent: :destroy
+    
     has_secure_password
-
-    validates :email, presence: true
+    validates :email, presence: true, uniqueness: true
     validates :username, presence: true, uniqueness: true
-    validates :password, presence: true
 end
