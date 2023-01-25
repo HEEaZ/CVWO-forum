75d3efa7f513e900ee2e51a42cbfbfadad20b135
Imman <do.u.noe.imm@gmail.com>
2023-01-25 11:43:52 +0800

    Reconfigured login to implementation using username instead of email

--- backend/app/controllers/authentication_controller.rb
+++ backend/app/controllers/authentication_controller.rb
@@ -2,7 +2,7 @@ class AuthenticationController < ApplicationController
     skip_before_action :authenticate_request
 
     def login
-        @user = User.find_by_email(params[:email])
+        @user = User.find_by_username(params[:username])
         if @user&.authenticate(params[:password])
             token = jwt_encode(user_id: @user.id)
             render json: { token: token }, status: :ok
