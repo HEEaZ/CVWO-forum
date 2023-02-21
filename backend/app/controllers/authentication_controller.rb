5b36f3e4c613cad6f737a4663d3b5def3968d991
Imman <do.u.noe.imm@gmail.com>
2023-02-22 04:05:35 +0800

    fixed user json return on login

--- backend/app/controllers/authentication_controller.rb
+++ backend/app/controllers/authentication_controller.rb
@@ -5,7 +5,7 @@ class AuthenticationController < ApplicationController
         @user = User.find_by_username(params[:username])
         if @user&.authenticate(params[:password])
             token = jwt_encode(user_id: @user.id)
-            render json: { token: token, user: @user.to_json(only: [:id, :username, :email]) }, status: :ok
+            render json: { token: token, user: @user.as_json(only: [:id, :username, :email]) }, status: :ok
         else
             render json: { error: 'Invalid Credentials' }, status: :unauthorized
         end
