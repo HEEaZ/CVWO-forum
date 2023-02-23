48f86a44066a9745bc30ca80dd0878b9af089566
Imman <do.u.noe.imm@gmail.com>
2023-02-24 00:00:04 +0800

    changed cors to *

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://main--tangerine-donut-c67ba3.netlify.app/"
+    origins "*"
 
     resource "*",
       headers: :any,
